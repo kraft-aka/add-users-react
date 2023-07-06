30793db8966fcf5475744f0a579c8cf75c436950
kubat <kubat.aiupov@gmail.com>
2023-07-06 20:46:25 +0200

    add portals to ErrorModal

--- src/components/ErrorModal.jsx
+++ src/components/ErrorModal.jsx
@@ -1,10 +1,28 @@
 import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import "./ErrorModal.css";
 
+function Backdrop(props) {
+  return <div className="backdrop" onClick={props.clickHandlerModal}></div>;
+}
+
+function ModalOverlay(props) {
+  return (
+    <Card className="modal">
+      <div className="header">
+        <h2>{props.title}</h2>
+      </div>
+      <p className="content">{props.message}</p>
+      <footer className="actions">
+        <Button onClick={props.clickHandlerModal}>OKAY!</Button>
+      </footer>
+    </Card>
+  );
+}
+
 export default function ErrorModal(props) {
-  // TODO: function to hide modal
   function clickHandlerModal(e) {
     props.closeModal();
   }
@@ -20,18 +38,19 @@ export default function ErrorModal(props) {
   }, []);
 
   return (
-    <div className="backdrop" onClick={clickHandlerModal}>
-      <Card className="modal">
-        <div className="header">
-          <h2>{props.title}</h2>
-        </div>
-
-        <p className="content">{props.message}</p>
-
-        <Button className="actions" onClick={clickHandlerModal}>
-          OKAY!
-        </Button>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.clickHandlerModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onClick={props.clickHandlerModal}
+        />,
+        document.getElementById("modal-overlay-root")
+      )}
+    </>
   );
 }
